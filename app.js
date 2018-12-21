01679c85766077216472eb30ff3dcf968b90b7f2
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2018-12-20 23:14:15 -0800

    It has performed all the required activities on the page.

--- app.js
+++ app.js
@@ -2,19 +2,54 @@ $(document).ready(function(){
   console.log('jQuery loaded');
 
   // write to local storage from input when button save clicked
-  $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+//   $('.btn-submit').on('click', function(){
+//     localStorage.setItem('inputFieldValue', $('.text-entry').val());
+//     var myItemInStorage = localStorage.getItem('inputFieldValue');
+//     console.log('myItemInStorage', myItemInStorage);
 
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
+//     // display the value here
+//     $('.list-display-field').text(myItemInStorage); // ??
 
+//   });
+
+//   // delete from local storage when delete button clicked
+//   $('.btn-delete').on('click', function(){
+//     localStorage.removeItem('inputFieldValue');
+//   });
+   var selectedItems = [];
+  $('.addcart').click(function(){
+   console.log('clicked');
+
+
+   $('.selectionform input:checked').each(function(){
+     var inputvalue = $(this).val();
+     selectedItems.push([inputvalue]);
+   });
+  console.log(selectedItems);
+  });
+  
+  $('.checkout').click(function(){
+    localStorage.setItem(ol ,JSON.stringify(selectedItems));
+    alert("Thank you for shopping with us!");
   });
 
-  // delete from local storage when delete button clicked
-  $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
-  });
+ var orderkey = function(){
+      if(localStorage.length === 0){
+      return 'order1';
+    }
+   else {
+    var keysObj = Object.keys(localStorage);
+    var lastk = keysObj[keysObj.length-1];
+    var partkey = lastk.slice(-1);
+    var numk = Number(partkey)+1;
 
-});
+    return 'order'.concat(numk.toString());
+  }
+}
+var ol = orderkey(); 
+
+ $('.delete').click(function(){
+  localStorage.clear();
+ });
+ });
+
